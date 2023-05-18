c7c15943749ee5899dd2e6d2745d0d5087a6d7ee
Manuel Saneh <man.saneh10@gmail.com>
2023-05-18 21:21:26 +0300

    updated

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,12 +8,12 @@ import { fadeIn, textVariant } from "../utils/motion"
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link, web_link }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full h-full">
-        <div className="relative w-full">
+    // <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        <div className="relative w-full h-[200px]">
           <img 
             src={image}
-            alt={name}
+            alt="project-image"
             className="w-full h-full object-cover rounded-2xl"
           />
 
@@ -58,31 +58,33 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
           </a>
         </div>
       </div>
-    </motion.div>
+    // </motion.div>
   )
 }
 
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      {/* <motion.div variants={textVariant()}> */}
+      <div>
         <p className={styles.sectionSubText}>
           My Work
         </p>
         <h2 className={styles.sectionHeadText}>
           Projects.
         </h2>
-      </motion.div>
+      </div>
+      {/* </motion.div> */}
 
       <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+        <p
+          // variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through real-world examples of my work. 
           Each project is briefly described with links to code repositories and live demos in it. It reflects
           my ability to solve complex problems, work with different technologies, and manage projects effectively.
-        </motion.p>
+        </p>
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
